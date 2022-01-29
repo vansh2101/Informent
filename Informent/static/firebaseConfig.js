a7bb718480d76490e644772eb2a8c521238c6694
VANSH  SACHDEVA <68803315+vansh2101@users.noreply.github.com>
2022-01-29 23:36:16 +0530

    Update firebaseConfig.js

--- Informent/static/firebaseConfig.js
+++ Informent/static/firebaseConfig.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDvdE0tq3wtqh2s6XrG7Co31pSZZj9j_9Q",
-  authDomain: "transformbin.firebaseapp.com",
-  projectId: "transformbin",
-  storageBucket: "transformbin.appspot.com",
-  messagingSenderId: "322767065668",
-  appId: "1:322767065668:web:5de11112cadc8ebabb2211"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
